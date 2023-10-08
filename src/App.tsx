a2378b64e8a72a9f40bb2911c0c80e68e579a249
allongsio <choisio2301@gmail.com>
2023-10-09 04:36:08 +0900

    [feat]Add Arrow Icons for Display and Apply Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,72 @@
 import React, { useState } from "react";
 import Layout from "./components/Layout";
+import sceneData from "./scene.json";
+import { styled } from "styled-components";
 
 function App() {
   // 현재 몇번 사진인지(초기값은 파일명 1.jpeg)
   const [sceneNumber, setSceneNumber] = useState(1);
 
+  // 파일명 번호에서 1을 뺀 index번째의 scene
+  const currentScene = sceneData.scene[sceneNumber - 1];
+
+  // 해당 scene이 존재할 때, 해당 scene 위에 표시될 화살표 아이콘 태그들을 생성하는 핸들러 함수
+  const renderHitzones = () => {
+    if (!currentScene || !currentScene.hitzones) {
+      return null;
+    } else {
+      return currentScene.hitzones.map((ele, index) => {
+        // ele.goto는 scene에 따라서 있기도 하고, 없기도 하기 때문에 typescript에서는 다음과 같이 명시해줄 필요가 있음
+        if (ele.form === "up" && ele.goto) {
+          return (
+            <Hitzone
+              className='bi bi-arrow-up-circle-fill'
+              key={index}
+              form={ele.form}
+              x={ele.x}
+              y={ele.y}
+            ></Hitzone>
+          );
+        } else if (ele.form === "left") {
+          return (
+            <Hitzone
+              className='bi bi-arrow-left-circle-fill'
+              key={index}
+              form={ele.form}
+              x={ele.x}
+              y={ele.y}
+            ></Hitzone>
+          );
+        } else {
+          return (
+            <Hitzone
+              className='bi bi-arrow-right-circle-fill'
+              key={index}
+              form={ele.form}
+              x={ele.x}
+              y={ele.y}
+            ></Hitzone>
+          );
+        }
+      });
+    }
+  };
+
   return (
     <div className='App'>
       <Layout>
         <img alt='scene' src={`/images/${sceneNumber}.jpeg`} />
+        {renderHitzones()}
       </Layout>
     </div>
   );
 }
 
 export default App;
+
+const Hitzone = styled.i<{ form: string; x: string; y: string }>`
+  ${({ form }) =>
+    form === "up" ? `position : absolute;` : `position : fixed;`}
+  font-size: 70px;
+  ${({ x, y }) => `left:${x}; top:${y}`}
+`;
