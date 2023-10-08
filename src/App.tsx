e795904dc250b874dc37ea1a96ae98aa66becba1
allongsio <choisio2301@gmail.com>
2023-10-09 04:27:18 +0900

    [feat]Integrate Layout component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "./components/Layout";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <Layout>hi</Layout>
+    </div>
+  );
 }
 
 export default App;
