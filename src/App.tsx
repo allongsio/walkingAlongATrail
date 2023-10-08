280152c2c8ef8a8a78edac4ca0486899fc309701
allongsio <choisio2301@gmail.com>
2023-10-09 04:31:41 +0900

    [feat]Define State for Rendering Current Scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./components/Layout";
 
 function App() {
+  // 현재 몇번 사진인지(초기값은 파일명 1.jpeg)
+  const [sceneNumber, setSceneNumber] = useState(1);
+
   return (
     <div className='App'>
-      <Layout>hi</Layout>
+      <Layout>
+        <img alt='scene' src={`/images/${sceneNumber}.jpeg`} />
+      </Layout>
     </div>
   );
 }
